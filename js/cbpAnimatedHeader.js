b16ec9905af24e30ebcda4e6cc75c6772c884efd
Antonio Trento <antoniotrento83@gmail.com>
2016-10-25 09:38:27 +0200

    s

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -11,7 +11,7 @@
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-default' ),
+		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
 		changeHeaderOn = 300;
 
